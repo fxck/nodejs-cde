d7d7c318b16649b068d7e9e81691f726df0163ca
root <root@node-id-3.runtime.ales.zerops>
2025-05-03 19:46:11 +0000

    cde

--- install-vscode-server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Create the target directory
-mkdir -p ~/.vscode-server/bin
-
-# Download and execute the installation script
-wget -O- https://aka.ms/install-vscode-server/setup.sh | sh
